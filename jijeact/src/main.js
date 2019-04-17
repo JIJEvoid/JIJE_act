786bf3a7c71aad7ec71382940ab746a7dc3479cb
JIJEvoid <407039579@qq.com>
2019-04-17 15:44:47 +0800

    add

--- jijeact/src/main.js
+++ jijeact/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+import VueFullPage from 'vue-fullpage.js'
+import App from './App'
 
-Vue.config.productionTip = false
+Vue.use(VueFullPage);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
